f77adee0731719c01fde26559733275337737ae9
Santosh T <bt.santosh01@gmail.com>
2022-06-10 16:26:18 +0100

    fix the logic for additional fuel calculation

--- lib/space_center.ex
+++ lib/space_center.ex
@@ -1,39 +1,48 @@
 defmodule SpaceCenter do
   @moduledoc false
 
-  def fuel_required(mass, directives, fuel_weight \\ 0) when mass > 0 do
+  def fuel_required(mass, _launch_gravity) when mass <= 0, do: nil
+
+  def fuel_required(mass, directives) when mass > 0 do
     Enum.map(directives, fn directive ->
-      IO.inspect(directive)
       launch_gravity = if elem(directive, 0) == :launch, do: elem(directive, 1), else: 0
       land_gravity = if elem(directive, 0) == :land, do: elem(directive, 1), else: 0
 
       fuel_for_launch = if launch_gravity > 0, do:
                           round(mass * launch_gravity * 0.042 - 33), else: 0
-      total_launch_fuel = if fuel_for_launch > 0 do
-        additional_launch_fuel = fuel_for_launch + fuel_weight
+
+      total_launch_fuel = if fuel_for_launch > 0, do:
         calculate_launch_fuel(fuel_for_launch, launch_gravity)
-        additional_launch_fuel
-      end
 
       fuel_for_land = if land_gravity > 0, do:
-                      round(mass * land_gravity * 0.033 - 42), else: 0
-      total_land_fuel = if fuel_for_land > 0 do
-        additional_land_fuel = fuel_for_land + fuel_weight
-        calculate_land_fuel(fuel_for_land, land_gravity)
-        additional_land_fuel
-      end
+              round(mass * land_gravity * 0.033 - 42), else: 0
+
+      total_land_fuel = if fuel_for_land > 0, do:
+      calculate_land_fuel(fuel_for_land, land_gravity)
 
       if total_launch_fuel, do: total_launch_fuel, else: total_land_fuel
     end)
   end
 
-  def fuel_required(mass) when mass <= 0, do: nil
-
-  defp calculate_launch_fuel(fuel_for_launach, launch_gravity) do
-    round(fuel_for_launach * launch_gravity * 0.042 - 33)
+  defp calculate_launch_fuel(fuel_for_launch, launch_gravity) do
+    directive = {:launch, launch_gravity}
+    gravity = elem(directive, 1)
+    total_mass = round(fuel_for_launch * gravity * 0.042 - 33)
+    additional_fuel = unless total_mass <= 0 do
+      calculate_launch_fuel(total_mass, launch_gravity)
+      fuel_for_launch + total_mass
+    end
+    additional_fuel
   end
 
   defp calculate_land_fuel(fuel_for_land, land_gravity) do
-    round(fuel_for_land * land_gravity * 0.033 - 42)
+    directive = {:land, land_gravity}
+    gravity = elem(directive, 1)
+    total_mass = round(fuel_for_land * gravity * 0.033 - 42)
+    additional_fuel = unless total_mass <= 0 do
+      calculate_land_fuel(total_mass, land_gravity)
+      fuel_for_land + total_mass
+    end
+    additional_fuel
   end
 end
--- test/space_center_test.exs
+++ test/space_center_test.exs
@@ -5,20 +5,17 @@ defmodule SpaceCenterTest do
   test "check the space craft programs" do
     apollo_mass = 28801
     apollo_directives = [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 9.807}]
+    fuel_for_apollo = SpaceCenter.fuel_required(apollo_mass, apollo_directives)
+    assert fuel_for_apollo == [16670, 1536, 2025, 12240]
 
     mars_mass = 14606
     mars_directives = [{:launch, 9.807}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}]
+    fuel_for_mars = SpaceCenter.fuel_required(mars_mass, mars_directives)
+    assert fuel_for_mars == [8414, 1919, 2561, 6159]
 
     pass_ship_mass = 75432
     ship_directives = [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}]
-
-    fuel_for_apollo = SpaceCenter.fuel_required(apollo_mass, apollo_directives)
-    assert fuel_for_apollo == [11830, 1498, 1927, 9279]
-
-    fuel_for_mars = SpaceCenter.fuel_required(mars_mass, mars_directives)
-    assert fuel_for_mars == [5983, 1747, 2244, 4685]
-
     fuel_for_pass_ship = SpaceCenter.fuel_required(pass_ship_mass, ship_directives)
-    assert fuel_for_pass_ship == [31037, 3991, 5099, 9196, 11724, 24370]
+    assert fuel_for_pass_ship == [43788, 4162, 5413, 10280, 13518, 32215]
   end
 end
